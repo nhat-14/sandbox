e316d90491c39d9f8fcd07098d9b8b936522f2d5
Ubuntu <azureuser@wfm-arm64.1frep2vdxbne1ijchxs0qn1mbc.lx.internal.cloudapp.net>
2026-09-03 05:57:03 +0000

    change go verson

--- scripts/modules/go.sh
+++ scripts/modules/go.sh
@@ -16,14 +16,14 @@ install_go() {
   
   if command_exists go; then
     GO_VERSION="$(go version | cut -d ' ' -f 3 | cut -c 3-)"
-    echo "⚡️ Go ${GO_VERSION} already installed, skipping installation"
+      echo "⚡️ Go ${GO_VERSION} already installed, skipping installation"
   else
-    sudo rm -rf /usr/local/go /usr/bin/go
-    wget "https://go.dev/dl/go1.25.10.linux-${CPU_ARCH}.tar.gz" -O go.tar.gz
-    sudo tar -C /usr/local -xzf go.tar.gz
-    rm go.tar.gz
-    which go
-    go version
-    echo "✅ Go installed"
+  sudo rm -rf /usr/local/go /usr/bin/go
+  wget "https://go.dev/dl/go1.26.5.linux-${CPU_ARCH}.tar.gz" -O go.tar.gz
+  sudo tar -C /usr/local -xzf go.tar.gz
+  rm go.tar.gz
+  which go
+  go version
+  echo "✅ Go installed"
   fi
 }
